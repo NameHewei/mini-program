60db67c7ddc276be11b8716f9b22d237591291f4
NameHewei <heweimailbox@foxmail.com>
2019-09-29 17:17:41 +0800

    regular update

--- miniprogram/pages/menu/menu.js
+++ miniprogram/pages/menu/menu.js
@@ -5,7 +5,28 @@ Page({
    * 页面的初始数据
    */
   data: {
-    scanResult: '-'
+    scanResult: '-',
+    currentPage: 1,
+    dataList: [],
+    pickerValue: 0,
+    objectArray: [
+      {
+        id: 'a',
+        name: '美国'
+      },
+      {
+        id: 'c',
+        name: '中国'
+      },
+      {
+        id: 'b',
+        name: '巴西'
+      },
+      {
+        id: 'b',
+        name: '日本'
+      }
+    ]
   },
 
   /**
@@ -19,7 +40,18 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
+    console.log('attached')
+    const arr = [];
 
+    for (let i = 0; i < 12; i++) {
+      arr.push({
+        id: 'id' + i, num: `num:${i}`, cd: [{ id: 11, num: '121233', name: 'namemem' }]
+      })
+    }
+
+    this.setData({
+      dataList: arr
+    })
   },
 
   /**
@@ -47,14 +79,20 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    this.setData({
+      currentPage: 1
+    })
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    this.setData({
+      currentPage: this.data.currentPage + 1,
+      dataList: [...this.data.dataList, {id: 'id' + 10, num: `num:10`, cd: [{ id: 13, num: '01213233', name: '0namemem' }]}]
+    })
+    console.log(this.data.currentPage)
   },
 
   /**
@@ -63,6 +101,14 @@ Page({
   onShareAppMessage: function () {
 
   },
+
+  bindPickerChange: function(e) {
+    console.log('picker发送选择改变，携带值为', e)
+    this.setData({
+      pickerValue: e.detail.value
+    })
+  },
+
   /**
    * 扫描二维码
    */
